f13869216c8dc27b4b05500b707efabc8832976e
swansavage@github.com <swansavage@gmail.com>
2024-10-10 17:27:35 -0500

    fix

--- netlify/functions/fetchNHLRoster.js
+++ netlify/functions/fetchNHLRoster.js
@@ -12,6 +12,7 @@ exports.handler = async function (event, context) {
 		}
 
 		const data = await response.json();
+		console.log(data);
 
 		return {
 			statusCode: 200,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
-	content: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
+	content: [
+		'./src/**/*.{html,js,ts,jsx,tsx,astro}', // Adjust paths based on your project structure
+	],
 	theme: {
 		extend: {},
+	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#0038A8', // Rangers blue
+					secondary: '#CE1126', // Rangers red
+					accent: '#FFFFFF', // White (neutral)
+					neutral: '#F0F0F0', // Light gray for neutral backgrounds
+					'base-100': '#FFFFFF', // Base white background
+					info: '#005BB7', // Slightly darker blue for info
+					success: '#4CAF50', // Success green (optional)
+					warning: '#FFC107', // Warning yellow
+					error: '#E53935', // Error red
+				},
+			},
+		],
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')], // Add DaisyUI here
 };
